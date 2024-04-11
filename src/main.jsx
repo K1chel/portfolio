4069b3add5026620f901ed80f808ab1e3ad2e378
Your Name <cherevko022@gmail.com>
2024-04-10 20:48:41 -0400

    Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
 import { Toaster } from "sonner";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
+import "./index.css";
+
 import { ModalProviders } from "./providers/modal-providers.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
       <ModalProviders />
       <Toaster theme="dark" />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
